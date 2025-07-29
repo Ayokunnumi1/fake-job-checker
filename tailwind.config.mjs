86a68d2df71eb3a42396896ea734c6ba77d0fd7d
Ayokunnumi1 <omololuayk@gmail.com>
2025-07-29 14:28:20 +0100

    Remove unnecessary code

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 /** @type {import('tailwindcss').Config} */
 const config = {
-  content: [
-    "./src/app/**/*.{js,ts,jsx,tsx}",
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("@tailwindcss/forms"), require("tailwind-scrollbar")],
 };
 
 export default config;
